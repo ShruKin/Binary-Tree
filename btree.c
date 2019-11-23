02bce15cce49cbefc58946dcd0c5b941b4d8a5f3
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2019-11-23 17:31:53 +0530

    Added BFS + printGivenLevel - prints a single level + printLevelOrder - print the whole BFS Level wise

--- btree.c
+++ btree.c
@@ -27,6 +27,27 @@ struct Btree* create_btree_recur(){
     return root;
 }
 
+void printGivenLevel(struct Btree *root, int level) 
+{ 
+    if (root == NULL) 
+        return; 
+    if (level == 1) 
+        printf("%d ", root->data); 
+    else if (level > 1) 
+    { 
+        printGivenLevel(root->left, level-1); 
+        printGivenLevel(root->right, level-1); 
+    } 
+} 
+
+void printLevelOrder(struct Btree *root) // BFS
+{ 
+    int h = height(root); 
+    int i; 
+    for (i=1; i<=h; i++) 
+        printGivenLevel(root, i); 
+} 
+
 void preorder(struct Btree *root)
 {
 	if(root != NULL)
